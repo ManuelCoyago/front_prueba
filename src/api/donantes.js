ffbd9a673d72d1efb59283e26d85b1b6a594d572
ManuelCoyago <133995076+ManuelCoyago@users.noreply.github.com>
2025-02-02 17:43:12 -0500

    plug and pray

--- src/api/donantes.js
+++ src/api/donantes.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/donantes', // Asegúrate de que el backend esté corriendo en este puerto
+  baseURL: 'https://back-prueba-tau.vercel.app/api/donantes', // Asegúrate de que el backend esté corriendo en este puerto
 });
 
 export default api;
